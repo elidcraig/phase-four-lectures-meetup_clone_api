1d5e9816da4e7535af963030c927a66ccea0b934
Eli Craig <elidcraig@gmail.com>
2022-06-30 11:43:25 -0700

    seed data, faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+8.times do
+  User.create(Faker::Internet.user('username', 'email'))
+end
+
+Group.create(name: 'Callback Maybe',location: 'here')
+Group.create(name: 'Fetch em All',location: 'here')
+
+Event.create(
+  title: 'Happy Hour',
+  description: 'I feel fucking crazy!',
+  location: 'Wherever you are',
+  starts_at: '2022-06-30 20:00:00',
+  ends_at: '2022-06-30 22:00:00',
+  group: Group.first,
+  user: User.last
+)
